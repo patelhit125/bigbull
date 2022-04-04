2a1153ffb8d04d3e9f04e1ca56059dcecbf41e12
Hit Patel <patelhit125@gmail.com>
2022-04-04 21:03:39 +0530

    Updates

---
+++ precache-manifest.0e2e9cab7c36a7d86c0282606c9934ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d512a6578ebb41dcbeff58950b90b44f",
+    "url": "/bigbull/index.html"
+  },
+  {
+    "revision": "c57467e9bf3d614f2074",
+    "url": "/bigbull/static/css/main.4ca9dbfa.chunk.css"
+  },
+  {
+    "revision": "d0733b5dbe07f767e76c",
+    "url": "/bigbull/static/js/2.828919bf.chunk.js"
+  },
+  {
+    "revision": "2bc9c08637eae4a3fa0b97a7d6f932b6",
+    "url": "/bigbull/static/js/2.828919bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57467e9bf3d614f2074",
+    "url": "/bigbull/static/js/main.0c7173e8.chunk.js"
+  },
+  {
+    "revision": "9ebc68efd816c96f99e1",
+    "url": "/bigbull/static/js/runtime-main.d3fc5670.js"
+  },
+  {
+    "revision": "7ee9c3d762c31974f2675e010f2b9026",
+    "url": "/bigbull/static/media/bigbull.7ee9c3d7.png"
+  },
+  {
+    "revision": "159219b6bbedacafb2d7d8a6dec78af8",
+    "url": "/bigbull/static/media/land.159219b6.svg"
+  }
+]);
